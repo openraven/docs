6e3bd5c4389d6c9cf4b6023408207b438c377328
Mark Curphey <curphey@users.noreply.github.com>
2021-10-22 07:57:11 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Open Raven Research'
+copyright = '2021, Open Raven'
+author = 'Open Raven Resarch Team'
 
 release = '0.1'
 version = '0.1.0'
